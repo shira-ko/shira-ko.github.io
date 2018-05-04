8ea7f71a8f34bbe032673e384a24ade7adc19047
shira-ko <shira-ko@fablabkitakagaya.org>
2018-05-04 20:33:34 +0900

    add script

---
+++ assets/js/randomSelect.js
@@ -0,0 +1,8 @@
+var hh = document.getElementById("wrapper");
+  //stusList = students.getElementsByTagName('li');
+
+console.log(hh);
+
+/*for (var i = 0; stusList.lemgth; i++) {
+  console.log(stusList[i].textContent);
+}*/
