a7e2653aaf01eb466652ef19626bb4f4a6f278be
shira-ko <shira-ko@fablabkitakagaya.org>
2018-10-27 23:03:16 +0900

    add testhtml

---
+++ test/try1.js
@@ -0,0 +1,11 @@
+function setup(){
+  ////描画範囲指定
+  createCanvas(windowWidth, windowHeight);
+  //背景色指定
+  background('gray');
+  //円描画（位置指定x軸,位置指定y軸,円の縦幅指定,円の横幅指定）
+  ellipse(windowWidth/2, windowHeight/2, 72, 72);
+}
+
+function draw(){
+}
